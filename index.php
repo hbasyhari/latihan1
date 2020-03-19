fff03100e79cda95f570ff5336143a97a1757c8d
hbasyhari <habib.asyhari31@gmail.com>
2020-03-19 20:35:52 +0700

    Tugas8

--- index.php
+++ index.php
@@ -1,27 +1,69 @@
   <?php
-  
-  $x = 10 ;
-  $x += 20;
-  echo $x </br>;
 
-  $y = 15 ;
-  $y -= 8;
-  echo $y </br>;
+  var_dump(1 < 5);
+  	echo "</br>";
 
-  $y = 15 ;
-  $y = 8;
-  echo $y</br> ;
+  	 var dump(1 > 5);
+  	echo "</br>";
 
-  $y = 15 ;
-  $y /= 8;
-  echo $y </br>;
+  	 var dump(1 <= 5);
+  	echo "</br>";
 
-  $y = 15 ;
-  $y %= 8;
-  echo $y </br> ;
+  	 var dump(1 >= 5);
+  	echo "</br>";
 
-  $y = 15 ;
-  $y .= 8;
-  echo $y </br>;
+  	 var dump(1 == 5);
+  	echo "</br>";
+
+  	 var dump(1 != 5);
+  	echo "</br>";
+
+
+var_dump (1 === "1");
+
+var_dump (2 === "2");
+
+var_dump (3 === "3");
+
+var_dump (4 === "4");
+
+var_dump (5 === "5");
+
+var_dump (6 === "6");
+
+var_dump (7 === "7");
+
+var_dump (8 !== "8");
+
+
+$x = 10;
+var_dump($x < 15 && $x % 3 == 0);
+
+$x = 10;
+var_dump($x < 12 && $x % 2 == 0);
+
+$x = 20;
+var_dump($x < 9 && $x % 2 == 0);
+
+$x = 13;
+var_dump($x < 7 && $x % 3 == 0);
+
+$x = 4;
+var_dump($x < 12 && $x % 4 == 0);
+
+$x = 5;
+var_dump($x < 1 && $x % 2 == 0);
+
+$x = 23;
+var_dump($x < 20 && $x % 12 == 0);
+
+$x = 11;
+var_dump($x < 5 && $x % 2 == 0);
+
+$x = 8
+var_dump($x < 10 && $x % 5 == 0);
+
+$x = 6;
+var_dump($x < 19 || $x % 2 == 0);
 
   ?>
