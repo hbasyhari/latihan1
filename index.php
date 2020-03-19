3a407bd6290a24f7bc51234e7ef3e13c2dc9cdc1
hbasyhari <habib.asyhari31@gmail.com>
2020-03-19 20:01:42 +0700

    Tugas4

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
   <?php
-
-
-  //  variabel dan tipe data
-  // variabel = digunakan untuk menyimpan data
-  // Aturan Variabel 'tidak boleh diawali dengan angka,tapi boleh mengandung angka'
- 
   $nama = "Muhammad Habib Asyhari";
+  
+  echo "Hallo, Selamat Datang $nama </br>"; #petik dua
+  echo 'Hallo, Selamat Datang $nama </br>'; #petik satu
+
   ?>
-
-<!DOCTYPE html>
-
-<html>
-<head>
-	<title>
-		Belajar PHP
-	</title>
-</head>
-<body>
-  <h1>Hallo, Selamat Datang <?php echo $nama; ?></h1>>
-
- </body>
-</html>
