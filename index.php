350b06eb99fa208c1a4b46c2082b6866f8d608d1
hbasyhari <habib.asyhari31@gmail.com>
2020-03-20 15:14:03 +0700

    Tugas7

--- index.php
+++ index.php
@@ -1,69 +1,34 @@
   <?php
 
-  var_dump(1 < 5);
-  	echo "</br>";
+  $x = 10;
+  $x += 2;
 
-  	 var dump(1 > 5);
-  	echo "</br>";
+  echo $x ; 
+  echo "<br>";
 
-  	 var dump(1 <= 5);
-  	echo "</br>";
+  $y = 12;
+  $y -= 3;
 
-  	 var dump(1 >= 5);
-  	echo "</br>";
-
-  	 var dump(1 == 5);
-  	echo "</br>";
-
-  	 var dump(1 != 5);
-  	echo "</br>";
+  echo $y;
+  echo "<br>";
 
 
-var_dump (1 === "1");
+  $y = 14;
+  $y /= 7;
 
-var_dump (2 === "2");
+  echo $y;
+  echo "<br>";
 
-var_dump (3 === "3");
+  $y = 17;
+  $y %= 6;
 
-var_dump (4 === "4");
+  echo $y;
+  echo "<br>";
 
-var_dump (5 === "5");
+  $y = 18;
+  $y .= 9;
 
-var_dump (6 === "6");
-
-var_dump (7 === "7");
-
-var_dump (8 !== "8");
-
-
-$x = 10;
-var_dump($x < 15 && $x % 3 == 0);
-
-$x = 10;
-var_dump($x < 12 && $x % 2 == 0);
-
-$x = 20;
-var_dump($x < 9 && $x % 2 == 0);
-
-$x = 13;
-var_dump($x < 7 && $x % 3 == 0);
-
-$x = 4;
-var_dump($x < 12 && $x % 4 == 0);
-
-$x = 5;
-var_dump($x < 1 && $x % 2 == 0);
-
-$x = 23;
-var_dump($x < 20 && $x % 12 == 0);
-
-$x = 11;
-var_dump($x < 5 && $x % 2 == 0);
-
-$x = 8
-var_dump($x < 10 && $x % 5 == 0);
-
-$x = 6;
-var_dump($x < 19 || $x % 2 == 0);
-
+  echo $y;
+  echo "<br>";
+  
   ?>
