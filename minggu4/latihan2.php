5b21aa9c9dc5f6cb2706226bd7187f239043b4b0
hbasyhari <habib.asyhari31@gmail.com>
2020-03-27 15:05:35 +0700

    Task_6

---
+++ minggu4/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+	<meta charset="UTF-8">
+	<title>Latihan Looping</title>
+</head>
+<body>
+	<h2>Membuat perulangan tabel</h2>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<?php
+		for ($b = 1; $b <= 3; $b++){
+			echo "<tr>";
+			for ($k=0; $k <=5 ; $k++) { 
+			echo "<td> $b,$k </td>"; 
+			}
+			echo "</tr>";
+		}
+		?>
+	</table>
+</body>
+</html>
