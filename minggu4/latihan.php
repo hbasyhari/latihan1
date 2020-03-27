31e1c844dec3501d92f464d0ed2ab5d36b9830b7
hbasyhari <habib.asyhari31@gmail.com>
2020-03-27 14:35:09 +0700

    Task_3

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -1,7 +1,10 @@
 <?php
 
-echo "<h2>Mengunakkan Perulangan For</h2>";
-for ($i = 1; $i < 6; $i++){
-	echo "Hai, saya urutan ke-$i <br>" ;
+$i = 0;
+while ($i < 5){
+
+	echo "Hai, Dunia! <br>";
+	$i++;
 }
+
 ?>
