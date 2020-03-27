9f40446773afc1cb7bae98907794e49549c4a31f
hbasyhari <habib.asyhari31@gmail.com>
2020-03-27 15:56:32 +0700

    Task_10

--- minggu4/latihan2.php
+++ minggu4/latihan2.php
@@ -8,9 +8,14 @@
 	<h2>Membuat perulangan tabel</h2>
 	<table border="1" cellpadding="10" cellspacing="0">
 		<?php
-		for ($b = 1; $b <= 10; $b++){
-			echo "<tr>";
-			for ($k=0; $k <=7 ; $k++) { 
+				for ($b = 1; $b <= 10; $b++){
+
+					if ($b % 2 === 1){
+						echo "<tr bgcolor= '#ff9999'>";
+					}
+					else 
+						echo "<tr>";
+			for ($k=1; $k <=8 ; $k++) { 
 			echo "<td> $b,$k </td>"; 
 			}
 			echo "</tr>";
