abbd941ca6f3010527bd8b1919701267098a3c0a
hbasyhari <habib.asyhari31@gmail.com>
2020-04-09 21:02:07 +0700

    task7

--- minggu4/minggu6/date.php
+++ minggu4/minggu6/date.php
@@ -1,3 +1,19 @@
 <?php
-echo date("l", strtotime('30 August 2030'));
+function salam($waktu, $nama)
+{
+	return "Selamat $waktu, $nama!";
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta  name="viewport"
+	content="with=device-width, initial-scale=1.0">
+	<title>Latihan Function</title>
+</head>
+<body>
+	<h1><?=salam("Pagi", "Habib") ?></h1>
+</body>
+</!DOCTYPE html>
+<html>
