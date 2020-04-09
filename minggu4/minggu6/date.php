4314b2180c0f6df05c89579a2be7f6ce890b4c11
hbasyhari <habib.asyhari31@gmail.com>
2020-04-09 21:13:07 +0700

    task8

--- minggu4/minggu6/date.php
+++ minggu4/minggu6/date.php
@@ -1,19 +1,10 @@
 <?php
-function salam($waktu, $nama)
+function perkalian($angka1, $angka2)
 {
-	return "Selamat $waktu, $nama!";
+	return $angka1*$angka2;
 }
+$hasil = perkalian(20,3);
+echo "Perkalian 20 x 3 adalah $hasil";
+echo "<br>";
+echo "Perkalian 9 x 2 ".perkalian(9,2);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta  name="viewport"
-	content="with=device-width, initial-scale=1.0">
-	<title>Latihan Function</title>
-</head>
-<body>
-	<h1><?=salam("Pagi", "Habib") ?></h1>
-</body>
-</!DOCTYPE html>
-<html>
