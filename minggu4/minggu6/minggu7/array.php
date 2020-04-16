e59aaaeaf4bdee6aeba2f9673f9c0ab0c01d4c2d
hbasyhari <habib.asyhari31@gmail.com>
2020-04-16 20:36:12 +0700

    task1

---
+++ minggu4/minggu6/minggu7/array.php
@@ -0,0 +1,19 @@
+<?php
+
+$hari = array("senin", "selasa","rabu");
+
+// var_dump($hari);
+// echo $hari[0];
+//print_r($hari);
+
+//echo "<br>" ;
+
+// $bulan = ["januari", "februari","maret"];
+//print_r($bulan);
+
+print_r($hari);
+$hari[] = "kamis";
+echo "<br>";
+print_r($hari)
+
+?>
